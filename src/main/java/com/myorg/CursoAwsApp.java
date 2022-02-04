5323a12ce825215cecbba7901486c1e13f9be4ef
Kaiofprates <kaioprates@outlook.com>
2022-02-03 22:45:10 -0300

    first commit

--- src/main/java/com/myorg/CursoAwsApp.java
+++ src/main/java/com/myorg/CursoAwsApp.java
@@ -3,6 +3,7 @@ package com.myorg;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.sqs.Queue;
 
 import java.util.Arrays;
 
@@ -10,31 +11,13 @@ public class CursoAwsApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new CursoAwsStack(app, "CursoAwsStack", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
+        VpcStack vpcStack = new VpcStack(app, "Vpc");
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
-                .env(Environment.builder()
-                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                        .region(System.getenv("CDK_DEFAULT_REGION"))
-                        .build())
-                */
+        ClusterStack clusterStack = new ClusterStack(app,"Cluster",vpcStack.getVpc());
+        clusterStack.addDependency(vpcStack);
 
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                .env(Environment.builder()
-                        .account("123456789012")
-                        .region("us-east-1")
-                        .build())
-                */
-
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                .build());
+        Service01Stack service01Stack = new Service01Stack(app, "Service01", clusterStack.getCluster());
+        service01Stack.addDependency(clusterStack);
 
         app.synth();
     }
